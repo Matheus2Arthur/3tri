90e7af01f3b8ea7fd8b209457181d6f52794dcec
Matheus2Arthur <127850843+Matheus2Arthur@users.noreply.github.com>
2023-10-24 08:54:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,15 +6,18 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 // Estrutura de repetição - Enquanto
-while(contador< listaDeTeclas.length) {
-    listaDeTeclas[contador].classList[1];
-    listaDeTeclas[contador].onclick = function(){
-tocaSom('#som_tecla_splash');
+for(contador< listaDeTeclas.length){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    console.log(idAudio);
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
     };
-    contador = contador + 1;
-     console.log(instrumento);
-    const instrumento = listaDeTeclas[contador].classList[1];
-   
+
+contador = contador + 1;
+console.log(contador);
 }
 
 
