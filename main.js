d14cebf0b4f2ff8c1e622221583f5d890428b33d
Matheus2Arthur <127850843+Matheus2Arthur@users.noreply.github.com>
2023-11-01 09:41:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();}
-
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
 // Estrutura de repetição - Enquanto
 for(let contador = 0;contador < listaDeTeclas.length;contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
+    console.log(idAudio);
+
     tecla.onclick = function(){
         tocaSom(idAudio);
 };
 tecla.onkeydown = function(){
+    console.log(evento.code);
+   if(evento.code ==='Enter'|| evento.code ==='Space'){
     tecla.classList.add('ativa');
-}
+   }
+};
 tecla.onkeyup = function(){
     tecla.classList.remove('ativa');
-}
+};
 console.log(contador);
+
+function tocaSom (idElementoAudio){
+    const elemento = document.querySelector(idElementoAudio);
+    if(elemneto === null){
+        alert('Elemento não encontrado');
+    }
+}
 }
 
 
