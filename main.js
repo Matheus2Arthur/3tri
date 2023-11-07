cd9daac28d0410ab5f6e478fb4492abff158b49d
Matheus2Arthur <127850843+Matheus2Arthur@users.noreply.github.com>
2023-11-07 08:52:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,29 @@
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();}
+    const elemento = document.querySelector(idElementoAudio).play();
+    if (elemento === null) {
+        alert('Elemento não encontrado');
+    }
+    if (elemento && elemento.localName === 'audio'){
+         elemento.play();
+    }else { 
+        alert('Elemento não encontrado');
+}
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
 // Estrutura de repetição - Enquanto
-for(let contador = 0;contador < listaDeTeclas.length;contador++) {
+for(contador< listaDeTeclas.length){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
-
     console.log(idAudio);
 
     tecla.onclick = function(){
         tocaSom(idAudio);
-};
-tecla.onkeydown = function(){
-    console.log(evento.code);
-   if(evento.code ==='Enter'|| evento.code ==='Space'){
-    tecla.classList.add('ativa');
-   }
-};
-tecla.onkeyup = function(){
-    tecla.classList.remove('ativa');
-};
-console.log(contador);
+    };
 
-function tocaSom (idElementoAudio){
-    const elemento = document.querySelector(idElementoAudio);
-    if(elemneto === null){
-        alert('Elemento não encontrado');
-    }
-}
+contador = contador + 1;
+console.log(contador);
 }
 
 
