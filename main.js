65be86ca16f8388ef7e45a4d9a8c85f676546ae7
Matheus2Arthur <127850843+Matheus2Arthur@users.noreply.github.com>
2023-12-05 08:43:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //a função que deve ser executada é tocaSom, mas que som? Nesse caso,sera o parâmetro idElementoAudio. Ou seja, o endereço da tecla será procurado no html e reproduzido.
 function tocaSom(idElementoAudio){
-    const elemento = document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
 //se o elemento pesquisado for igual a "inexistente", o alerta de elemento não encontrado aparecerá.
     if (elemento === null) {
         alert('Elemento não encontrado');
@@ -14,8 +14,6 @@ function tocaSom(idElementoAudio){
 }
 //querySelector encontra as informações da tecla pesquisada no documento
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-let contador = 0;
 // Estrutura de repetição - Enquanto
 //para que as teclas não sejam repetidas de maneira infinita, criamos o for, com valor de início 0, e valor final chamado de contar < listaDeTeclas.length, para que a repetição seja exatamente do tamanho da nossa lista, que é de 9 teclas.
 for(let contador = 0; contador < listaDeTeclas.length; contador++) {
@@ -30,15 +28,15 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     };
 //onkeydown significa "quando a tecla estiver pressionada", nesse caso uma função sera executada, e qual é a função? É a estilização de mudança de cor de botão quando as teclas Enter e Space forem apertadas
-    tecla.onkeydown = function () {
-        tecla.classList.add ('ativa');
+    tecla.onkeydown = function(evento) {
+        if(evento.code === 'Enter'||evento.code === 'Space'){
+        tecla.classList.add ('ativa');}
     }
 //onkeyup significa "quando a tecla estiver erguida, não pressionada", nesse caso, a função repetida.
 tecla.onkeyup = function () {
     tecla.classList.remove ('ativa');
 }
-
-contador = contador + 1;
+console.log(instrumento);
 console.log(contador);
 }
 
